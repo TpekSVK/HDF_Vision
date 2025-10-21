1987d776fba5f225a72885123ff396cf174284b0
TpekSVK <teplansky.t@gmail.com>
2025-10-21 11:26:53 +0200

    Add light presence check tool

--- app/services/tools/__init__.py
+++ app/services/tools/__init__.py
@@ -4,5 +4,12 @@ from .ssd import SSDTool
 from .mse import MSETool
 from .ncc import NCCTool
 from .edge import EdgeChangeTool
+from .light_presence import LightPresenceCheckTool
 
-__all__ = ["SSDTool", "MSETool", "NCCTool", "EdgeChangeTool"]
+__all__ = [
+    "SSDTool",
+    "MSETool",
+    "NCCTool",
+    "EdgeChangeTool",
+    "LightPresenceCheckTool",
+]
